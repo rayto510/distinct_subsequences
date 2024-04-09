3872dfd56c79435da5fbab77f068f1f6f8d9e946
Raymond To <rto@demandbase.com>
2024-04-09 11:21:04 -0700

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #[allow(dead_code)]
 fn distinct_subsequences(s: String, t: String) -> i32 {
     fn go(i: usize, j: usize, dp: &mut Vec<Vec<i32>>, s: &Vec<char>, t: &Vec<char>) -> i32 {
+        // exhausted t, we found a match
         if j == t.len() {
             return 1;
         }
+
+        // exhausted s, no match. t may still have some characters left
         if i == s.len() {
             return 0;
         }
+
+        // check in memo if we have already calculated this state
         if dp[i][j] != -1 {
             return dp[i][j];
         }
+
+        // make decisions to leave or take the current character
         let leave = go(i + 1, j, dp, s, t);
         let take = if s[i] == t[j] {
             go(i + 1, j + 1, dp, s, t)
@@ -17,7 +24,10 @@ fn distinct_subsequences(s: String, t: String) -> i32 {
             0
         };
 
+        // store the result in memo
         dp[i][j] = leave + take;
+
+        // return the result
         dp[i][j]
     }
 
@@ -44,6 +54,10 @@ mod tests {
         assert_eq!(
             distinct_subsequences("babgbag".to_string(), "bag".to_string()),
             5
+        );
+        assert_eq!(
+            distinct_subsequences("aabb".to_string(), "ab".to_string()),
+            4
         );
     }
 }
